6d9ea7a9f10140b2ebb518220246903e92f3c3ca
Stephen Clemmer <stephenjclemmer@gmail.com>
2023-05-15 13:54:15 -0700

    added a .env file but still not getting it to use port 3001 instead of 3002.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const notFound = require('./error-handlers/404');
 const errorHandler = require('./error-handlers/500');
+/* `const PORT = process.env.PORT || 3002;` is setting the port number for the server to listen on. It
+first checks if there is a port number specified in the environment variable `process.env.PORT`. If
+there is, it uses that port number. If not, it defaults to port number 3002. */
 const PORT = process.env.PORT || 3002;
 
 const app = express();
@@ -19,6 +22,9 @@ app.use(errorHandler);
 
 app.use('*', notFound);
 
+/**
+ * This function starts the server and listens for incoming requests on a specified port.
+ */
 function start(){
   app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 }
